c121dcd6656d0f67511c6fc22d59d8e03cf4bc53
OmkarD09 <omkardhakane09@gmail.com>
2025-10-19 09:13:58 +0530

    exports

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let args = process.argv;
 for (let i = 2; i < args.length; i++) {
     console.log("hello", args[i]);
 }
+
+const math = require('./math');
+console.log(math.add(2, 3));
+console.log(math.sub(2, 3));
+console.log(Math.floor(math.mul(2, 3)));
+console.log(Math.floor(math.div(2, 3)));
+
+//
